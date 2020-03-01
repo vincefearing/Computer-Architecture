b8a507e77bcc81c5af86289b8cc218a80981698b
Vince Fearing <vincefearing@gmail.com>
2020-03-01 02:12:00 -0800

    Worked on flag loop

--- homework2a/main.cpp
+++ homework2a/main.cpp
@@ -15,5 +15,18 @@ int main ()
 
 string add(string a, string b, int & flags)
 {
+    bool n = false;
+    bool z = false;
+    bool v = false;
+    bool c = false;
+    string result;
+
+    for(int i = 0; i < a.size(); ++i)
+    {
+        if(a.at(i) == '0' && b.at(i) == '0')
+        {
+            result.insert(0, 1, '0');
+        }
+    }
 
 }
