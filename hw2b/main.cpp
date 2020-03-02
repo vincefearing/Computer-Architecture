45a2983ca71b51efb1a3498613a0c3aaef7ae404
Vince Fearing <vincefearing@gmail.com>
2020-03-02 04:09:05 -0800

    Created function that counts 0s

--- hw2b/main.cpp
+++ hw2b/main.cpp
@@ -12,13 +12,14 @@ using namespace std;
 void show(unsigned n);
 void wohs(unsigned n);
 unsigned ones(unsigned n);
+unsigned zeroes(unsigned n);
 
 int main()
 {
     unsigned n = 256 + 12;
     //show(n);
     //wohs(n);
-    cout << "Number of ones: " << ones(n) << endl;
+    cout << "Number of ones: " << zeroes(n) << endl;
     return 0;
 }
 
@@ -100,4 +101,32 @@ unsigned ones(unsigned n)
         }
     }
     return ones;
+}
+
+unsigned zeroes(unsigned n)
+{
+    string r;
+    int count = 0;
+    unsigned zeros = 0;
+    for(int i = 32; i > 0; --i)
+    {
+        ++count;
+        if(n % 2 != 0)
+        {
+            r.insert(0, 1, '1');
+        }
+        else
+        {
+            r.insert(0, 1, '0');
+            ++zeros;
+        }
+        n /= 2;
+
+        if(count == 8 && i > 1)
+        {
+            r.insert(0, 1, ' ');
+            count = 0;
+        }
+    }
+    return zeros;
 }
