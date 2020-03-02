ea97991e39d4805a92c573bbb68da5fe4dea1b35
Vince Fearing <vincefearing@gmail.com>
2020-03-02 04:05:02 -0800

    Created function to counts 1's

--- hw2b/main.cpp
+++ hw2b/main.cpp
@@ -11,12 +11,14 @@ using namespace std;
 
 void show(unsigned n);
 void wohs(unsigned n);
+unsigned ones(unsigned n);
 
 int main()
 {
     unsigned n = 256 + 12;
     //show(n);
-    wohs(n);
+    //wohs(n);
+    cout << "Number of ones: " << ones(n) << endl;
     return 0;
 }
 
@@ -70,4 +72,32 @@ void wohs(unsigned n)
         }
     }
     cout << r << endl;
+}
+
+unsigned ones(unsigned n)
+{
+    string r;
+    int count = 0;
+    unsigned ones = 0;
+    for(int i = 32; i > 0; --i)
+    {
+        ++count;
+        if(n % 2 != 0)
+        {
+            r.insert(0, 1, '1');
+            ++ones;
+        }
+        else
+        {
+            r.insert(0, 1, '0');
+        }
+        n /= 2;
+
+        if(count == 8 && i > 1)
+        {
+            r.insert(0, 1, ' ');
+            count = 0;
+        }
+    }
+    return ones;
 }
